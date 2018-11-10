1893ea97d7dd6dac4d8942233d390399c9637fb7
Makoto Saiga <teatime24h@gmail.com>
2018-11-10 12:36:20 +0900

    add cesium

---
+++ cesiumView/main.js
@@ -0,0 +1,18 @@
+(function() {
+  "use strict";
+
+  var viewer = new Cesium.Viewer("cesium");
+
+  viewer.dataSources.add(
+    Cesium.KmlDataSource.load("../kml/tatsutaKodoRoute.kml")
+  );
+
+  viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(135.6895, 34.576, 2000),
+    orientation: {
+      heading: 0,
+      pitch: -0.8,
+      roll: 0
+    }
+  });
+})();
